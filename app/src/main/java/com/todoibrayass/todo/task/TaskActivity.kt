5045ce3d0364b4ae19e8c1289934b339d379e20a
ibrahima <ibrahima.dansoko@outlook.com>
2020-02-11 14:33:06 +0100

    terminated td4

--- app/src/main/java/com/todoibrayass/todo/task/TaskActivity.kt
+++ app/src/main/java/com/todoibrayass/todo/task/TaskActivity.kt
@@ -15,23 +15,17 @@ class TaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.task_activity)
-        val desc = descEditText
-        val title = titleEditText
         val task = intent?.getSerializableExtra("editTask") as? Task
-        var id = UUID.randomUUID().toString()
-        if (task != null ) {
-            id = task.id
-            title.setText(task.title)
-            desc.setText(task.description)
-        }
+        titleEditText?.setText(task?.title)
+        descEditText?.setText(task?.description)
         validateTask.setOnClickListener {
             val sendTask =
                 Task(
-                    id =id,
-                    title = title.text.toString(),
-                    description = desc.text.toString()
+                    id = task?.id ?: UUID.randomUUID().toString(),
+                    title = titleEditText.text.toString(),
+                    description = descEditText.text.toString()
                 )
-           val intentTask =  intent.putExtra("myTask", sendTask as Serializable)
+            val intentTask = intent.putExtra("myTask", sendTask as Serializable)
             setResult(Activity.RESULT_OK, intentTask)
             finish()
         }
